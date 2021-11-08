3468b478454bc6d8a7566a1edb52ddf1bac55bb4
Sergio <sergio.ivan.melgarejo@gmail.com>
2021-11-08 17:53:02 -0300

    fin del dia 8
    -comenze a implementar la logica del main en javascrip
    -se volvio a diseñar el formato del main

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,11 +4,16 @@
 
 const main = document.getElementById("main");
 let memorizacion;
+//Dato de las paginas a mostrar
+let datos = "nada";
+const divPaginas = document.querySelector(".main__paginas");
 
 //Eventos
 
 main.addEventListener("click", (e) => {
     let evento = e.target;
+
+    /* contraccion de secciones de main  */
 
     if ( evento.classList.contains("main__abrir-i") ) {
         let secionElejida = evento.parentNode.parentNode;
@@ -19,6 +24,28 @@ main.addEventListener("click", (e) => {
         }
     
         memorizacion = secionElejida ;
+    }
+
+
+    /* obtener las paginas */
+
+    if ( evento.classList.contains("main__abrir-i") ) {
+        if( datos === "nada" ) {
+            fetch("./Data/Data-paginas.json").then( res  => {
+                return res.json()
+            }).then( res => {
+                datos = res;
+                mostrarPaginas();
+            })
+        }
+        else mostrarPaginas();
+    }
+
+    /* filtro de paginas */
+
+    if ( evento.classList.contains("main__filtro-boton") ) {
+        let menufiltro = evento.parentNode.parentNode.children[1];
+        menufiltro.classList.toggle("filtro-abierto");
     }
 
     /* abrir footer */
@@ -33,10 +60,70 @@ main.addEventListener("click", (e) => {
         memorizacion = evento ;
     }
 
-    /* ___________ */
+});
 
-    if ( evento.classList.contains("main__filtro-boton") ) {
-        let menufiltro = evento.parentNode.parentNode.children[1];
-        menufiltro.classList.toggle("filtro-abierto");
-    }
-})
+const mostrarPaginas = () => {
+
+//obtiene dato
+
+//pasa filtro y ordena
+
+//si esta buscador aplica y reordena
+
+//muestra segun filtro y buscador
+
+
+}
+
+const crearPagina = ( pagina ) => {
+    
+    const divPagina = document.createElement("div");
+    const img = document.createElement("img");
+    const h3 = document.createElement("h3");
+    const divFooter = document.createElement("div");
+    const pLenguaje = document.createElement("p");
+    const spanLenguaje = document.createElement("span");
+    const a = document.createElement("a");
+    const pDificultad = document.createElement("p");
+    const spanDificultad = document.createElement("span");
+
+    divPagina.setAttribute("main__pagina");
+    img.setAttribute("main__pagina-img");
+    h3.setAttribute("main__pagina-titulo");
+    divFooter.setAttribute("main__pagina-footer");
+    pLenguaje.setAttribute("main__pagina-lenguaje");
+    spanLenguaje.setAttribute("main__pagina-lenguaje-span");
+    a.setAttribute("main__pagina-link");
+    pDificultad.setAttribute("main__pagina-dificultad");
+    spanDificultad.setAttribute("main__pagina-dificultad-span");
+
+    img.setAttribute("src", pagina.url);
+    img.setAttribute("alt", `imagen de ${pagina.titulo}`)
+
+    h3.textContent = pagina.titulo;
+
+    pLenguaje.textContent = "Lenguaje: ";
+    spanLenguaje.textContent = pagina.lenguaje;
+
+    a.textContent = "Link";
+
+    pDificultad.textContent = "Dificultad: ";
+    spanDificultad.textContent = pagina.dificultad;
+
+    
+
+
+
+}
+
+const buscador = () => {
+
+}
+
+const filtro = () => {
+
+}
+
+// const mostrarModaldeCarga = () => {
+
+// }
