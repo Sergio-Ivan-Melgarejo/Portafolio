46efc55c797cab8612bc730c5991b30b74fbc5e0
Sergio <sergio.ivan.melgarejo@gmail.com>
2021-12-02 11:27:29 -0300

    agrego mensaje de error para contacto

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -7,15 +7,16 @@ export const footer = document.getElementById("footer"),
     footerEmail = document.getElementById("footerEmail"),
     form = document.getElementById("form"),
     formMensaje = document.getElementById("formMensaje"),
-    formMensajeError = document.getElementById("formMensajeError");
+    formMensajeError = document.getElementById("formMensajeError"),
+    cerrarMsj = document.getElementById("cerrar-msj");
 
 // Funciones
 
 const cerrarMensaje = () => {
-    formMensajeError.children[0].classList.remove("open");
-    formMensajeError.children[1].classList.remove("open");
-    formMensajeError.children[2].classList.remove("open");
-    formMensajeError.classList.remove("open");
+    formMensajeError.children[0].classList.toggle("open");
+    formMensajeError.children[1].classList.toggle("open");
+    formMensajeError.children[2].classList.toggle("open");
+    formMensajeError.classList.toggle("open");
 }
 
 // Eventos
@@ -23,17 +24,23 @@ const cerrarMensaje = () => {
 addEventListener("DOMContentLoaded", () => {
     footerEmail.addEventListener("click", () => {
         email.classList.toggle("open");
+        if ( formMensajeError.getAttribute("data-msj") ) {
+            cerrarMensaje();
+        }
     })
 
     footer.addEventListener("submit", (e) => {
+
         if ( form.formNombre.value.length < 4 ) {
             e.preventDefault();
             formMensajeError.children[0].classList.add("open");
             formMensajeError.classList.add("open");
-            setTimeout(()=>{
-                cerrarMensaje()
-            }, 5000)
+            formMensajeError.setAttribute("data-msj", true);
         }
+        else{
+            formMensajeError.children[0].classList.remove("open");
+        }
+
         if ( 
             !form.formEmail.value.includes("@") || 
             !form.formEmail.value.includes(".com") || 
@@ -42,17 +49,20 @@ addEventListener("DOMContentLoaded", () => {
             e.preventDefault();
             formMensajeError.children[1].classList.add("open");
             formMensajeError.classList.add("open");
-            setTimeout(()=>{
-                cerrarMensaje()
-            }, 5000)
+            formMensajeError.setAttribute("data-msj", true);
         }
+        else{
+            formMensajeError.children[1].classList.remove("open");
+        }
+
         if ( form.formMensaje.value.length < 9 ) {
             e.preventDefault();
             formMensajeError.children[2].classList.add("open");
             formMensajeError.classList.add("open");
-            setTimeout(()=>{
-                cerrarMensaje()
-            }, 5000)
+            formMensajeError.setAttribute("data-msj", true);
+        }
+        else{
+            formMensajeError.children[2].classList.remove("open");
         }
     })
 })
